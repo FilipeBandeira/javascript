84ad05f222718abd4bd6330c50e56258113c6d77
Filipe Bandeira <filipebandeira2022@gmail.com>
2022-07-21 23:36:27 -0300

    Exercícios JS

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Será multado!`)
+}
+
+else {
+    console.log(`Você está dentro da velocidade permitida!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
