183fbded6113d0887821fce9641bcda4359a7ace
Filipe Bandeira <filipebandeira2022@gmail.com>
2022-07-25 12:23:47 -0300

    Exercícios - JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+
+// While, laço de repetição, onde, enquanto a condição satisfazer o laço irá acontecer, se não, irá sair
+// primeiro a condição e depois o teste
+/*while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+//Nesta outra forma, o do...while primeiro testa, para que depois venha a condição. "Faça... enquanto"
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 7 )
