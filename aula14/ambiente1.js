4bdc6a60a839d373a2b67e6f6053449c2110575d
Filipe Bandeira <filipebandeira2022@gmail.com>
2022-07-26 13:36:30 -0300

    Laços de repetição

---
+++ aula14/ambiente1.js
@@ -0,0 +1,7 @@
+//Outra forma de trabalhar com laços de repetição, quando se sabe uma quantidade finita
+// Para = for
+console.log(`Vai começar!`)
+for(var c = 1; c<= 10; c++) {
+    console.log(c)
+}
+console.log(`Fim!`)
