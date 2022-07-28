7aade0793ee0d963118e0b77154e8365b32959c5
Filipe Bandeira <filipebandeira2022@gmail.com>
2022-07-28 17:40:54 -0300

    Desafio

--- aula14/Desafio3/script.js
+++ aula14/Desafio3/script.js
@@ -1,1 +1,31 @@
-alert('Olá!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.Value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        // window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+        }
+        if (i < f) {
+                //Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            } 
+        } else {
+                //Contagem regressiva
+                for (let c = i; c >= f; c -= p) {
+                    res.innerHTML += `${c} \u{1F449}`
+                }
+            }
+            res.innerHTML += `\u{1F3C1}`
+    }
+}
