a91faed40fa07fae40fd515b3e5dd1a6daccf4a3
Filipe Bandeira <filipebandeira2022@gmail.com>
2022-07-28 11:45:18 -0300

    Ajustes

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,10 +1,12 @@
-var idade = 16
-console.log(`Você tem ${idade} anos.`)
+var agora = new Date()
+var hora = agora.getHours()
 
-if (idade < 16) {
-    console.log(`Não vota!`)
-} else if (idade < 18 || idade >= 65) {
-    console.log(`Voto opcional!`)
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora < 18) {
+    console.log('Boa tarde!')
 } else {
-    console.log(`Voto obrigatório!`)
+    console.log('Boa noite!')
 }
--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,31 +1,10 @@
-var agora = new Date()
-var diaSem = agora.getDay()
+var idade = 16
+console.log(`Você tem ${idade} anos.`)
 
-console.log(diaSem)
-
-switch(diaSem) {
-    case 0:
-        console.log('Domingo')
-        break
-    case 1:
-        console.log('Segunda')
-        break
-    case 2:
-        console.log('Terça')
-        break
-    case 3:
-        console.log('Quarta')
-        break
-    case 4:
-        console.log('Quinta')
-        break
-    case 5:
-        console.log('Sexta')
-        break
-    case 6:
-        console.log('Sábado')
-        break
-    default:
-        console.log('[ERRO] Dia inválido!')
-        break
+if (idade < 16) {
+    console.log(`Não vota!`)
+} else if (idade < 18 || idade >= 65) {
+    console.log(`Voto opcional!`)
+} else {
+    console.log(`Voto obrigatório!`)
 }
